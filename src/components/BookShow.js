4fda26d4b7aa33de73da6c125aac3e1ed77fd7db
VYankov7 <vyankov02@gmail.com>
2023-03-01 10:50:05 +0200

    Reading List Application

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,34 +1,32 @@
 // import "./src/index.css"
-import { useState } from "react";
+import { useState, useContext } from "react";
+import BooksContext from "../context/books";
 import BookEdit from "./BookEdit";
 
-function BookShow({ book, onDelete, onEdit }) {
+function BookShow({ book }) {
   const [showEdit, setShowEdit] = useState(false);
+  const { deleteBookById } = useContext(BooksContext);
 
   const handleDeleteClick = () => {
-    onDelete(book.id);
+    deleteBookById(book.id);
   };
 
   const handleEditClick = () => {
-    setShowEdit(!showEdit)
+    setShowEdit(!showEdit);
   };
 
-  const handleSubmit = (id, newTitle) => {
-    setShowEdit(false)
-    onEdit(id, newTitle)
+  const handleSubmit = () => {
+    setShowEdit(false);
   };
 
-  let content = <h3>{book.title}</h3>
+  let content = <h3>{book.title}</h3>;
   if (showEdit) {
-    content = <BookEdit onSubmit={handleSubmit} book={book}/>
+    content = <BookEdit onSubmit={handleSubmit} book={book} />;
   }
 
   return (
     <div className="book-show">
-      <img
-       alt="books"
-       src={`https://picsum.photos/seed/${book.id}/300/200`}
-      />
+      <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
       <div>{content}</div>
       <div className="actions">
         <button className="edit" onClick={handleEditClick}>
